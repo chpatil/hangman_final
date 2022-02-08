a907a1114c184b0de0098f6bd124d8214700e5bb
chipat <40916066+chpatil@users.noreply.github.com>
2022-02-08 17:47:19 +0000

    Completed with user improvements

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
+
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 print(art.logo)
 #Testing code
